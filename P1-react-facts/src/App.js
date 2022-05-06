fcdda3e45ee547e557b1602420e4ec0d4e30e20f
Aaron Garcia <97797254+aaron-garcia-7@users.noreply.github.com>
2022-05-06 11:02:24 -0600

    Create App.js

---
+++ P1-react-facts/src/App.js
@@ -0,0 +1,16 @@
+import "./styles/App.css";
+
+// Components
+import Nav from "./components/Nav";
+import Facts from "./components/Facts";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Facts />
+    </div>
+  );
+}
+
+export default App;
